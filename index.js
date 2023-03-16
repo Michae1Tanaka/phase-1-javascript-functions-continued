c2c20fcbc86fe150ae83fd1e7ffc4508c6588be7
Michael Tanaka <tanakam4214@gmail.com>
2023-03-15 18:12:49 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const saturdayFun = function (shenanigans = 'roller-skate') {
+    return `This Saturday, I want to ${shenanigans}!`;
+}
+const mondayWork = function (task = 'go to the office') {
+    return `This Monday, I will ${task}.`
+}
+const wrapAdjective = (function (flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+})
